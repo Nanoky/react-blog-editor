8b0d809b764782090eab781975d49d65934be4fc
Nanok <konancharlesyao@gmail.com>
2023-03-24 14:29:30 +0000

    example upgraded

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import {EditorBase} from 'react-blog-editor';
+import './App.css';
+import { EditorBase, ServiceProvider } from 'react-blog-editor';
 
-const App = () => {
-  return (
-    <div>
-      <EditorBase />
-    </div>
-  )
+function App() {
+    return (
+        <div className='App'>
+            <div>
+                <ServiceProvider unsplashKey={'Pr75Jod0wsrksOqFVKoSJzL9HjzShsOM5Lj_5eri2NA'}>
+                    <EditorBase />
+                </ServiceProvider>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
